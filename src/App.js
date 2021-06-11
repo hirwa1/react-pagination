2592501b5cbdce245f8e38189a5e896939b80208
hirwa1 <hirwadeveloper@gmail.com>
2021-06-11 11:10:02 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -5,8 +5,20 @@ export default function App() {
   return (
     <div>
       <h1>Hirwa Pagination</h1>
+      <br />
+
       <span>
-       
+        <a href=""> &#8592; </a>
+        <a href=""> 1 </a>
+        <a href=""> 2</a>
+        <a href=""> 3 </a>
+        <a href="" class="active">
+          {' '}
+          4{' '}
+        </a>
+        <a href=""> 5 </a>
+        <a href=""> 6 </a>
+        <a href=""> &#8594; </a>
       </span>
     </div>
   );
